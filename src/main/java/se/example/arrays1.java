b9567461627c9bc9b4a3eed5d78f592c25c698e7
davidappel <davappelgren@gmail.com>
2020-04-26 23:05:31 +0200

    Week 3 Assagnments Arrays

---
+++ src/main/java/se/example/arrays1.java
@@ -0,0 +1,13 @@
+package se.example;
+
+public class arrays1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[] {11, 4, 12};
+
+            System.out.println(numbers[0]);
+            System.out.println(numbers[1]);
+            System.out.println(numbers[2]);
+
+
+        }
+    }
---
+++ src/main/java/se/example/arrays2.java
@@ -0,0 +1,19 @@
+package se.example;
+
+public class arrays2 {
+
+    public static void indexOf(String[] args) {
+
+        //create array
+        int[] age = {12, 4, 5, 2, 5};
+
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Element at index " + i + ": " + age[i]);
+
+        }
+
+
+    }
+
+
+}
